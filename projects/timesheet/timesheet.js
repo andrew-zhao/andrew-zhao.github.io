65a51c41752e3b17f997cd8949a111d956818d9a
Andrew Zhao <azhao@blizzard.com>
2017-11-14 14:28:22 -0800

    Finish basic functionality for timesheet project

--- projects/timesheet/timesheet.js
+++ projects/timesheet/timesheet.js
@@ -24,8 +24,6 @@ function prettyTime(hours, minutes) {
     hours -= 12;
   }
   
-  console.log(minutes.toString());
-  
   if (minutes < 10) {
     minutes = minutes.toString().padStart(2, "0");
   }
@@ -38,10 +36,9 @@ function dateLoop() {
 		let date = new Date();
 		if (date.getSeconds() === 0) {
 			$("#currentTime").text(prettyTime(date.getHours(), date.getMinutes()));
+			updateMessage();
 		}
 	}, 1000);
-	
-	updateMessage();
 };
 
 function getLastItem(arr) {
@@ -81,6 +78,7 @@ function convertDateToMinutes(date) {
 }
 
 // Calculates total time worked and returns total minutes
+// Validation already performed in logTime
 // TODO: Loop and figure out time worked
 function calculateTimeWorked(timesheetLog, date) {
 	if (timesheetLog.length === 0) {
@@ -89,25 +87,73 @@ function calculateTimeWorked(timesheetLog, date) {
 		return convertDateToMinutes(date) - parseStoredTime(getLastItem(timesheetLog)[1]);
 	}
 	let total = 0;
-	for (i = 0; i < timesheetLog.length; i++) {
-		
+	for (i = 1; i < timesheetLog.length; i++) {
+		if (i % 2 === 1) {
+			total += (parseStoredTime(timesheetLog[i][1]) - parseStoredTime(timesheetLog[i-1][1]));
+		}
 	}
+	if (timesheetLog.length % 2 === 1) {
+		total += convertDateToMinutes(date) - parseStoredTime(getLastItem(timesheetLog)[1]);
+	}
+	return total;
+}
+
+// Check if a meal break was taken
+function mealBreakExists(timesheetLog) {
+	let breakStart = false;
+	let breakEnd = false;
+	for (i = 0; i < timesheetLog.length; i++) {
+		if (timesheetLog[i][0] === mealIn) {
+			breakEnd = true;
+		} else if (timesheetLog[i][0] === mealOut) {
+			breakStart = true;
+		}
+		if (breakStart && breakEnd) {
+			return true;
+		}
+	}
+	return false;
+}
+
+// Generate a message to estimate when to clock out at the end of the day
+function endOfDayMessage(hours, minutes, date, timesheetLog) {
+	let remainingMinutes = 60 - minutes;
+	let remainingHours = 8 - hours;
+	if (remainingMinutes > 0) {
+		remainingHours -= 1;
+	}
+	
+	if (!mealBreakExists(timesheetLog)) {
+		remainingMinutes += 30;
+	}
+	
+	let endOfDayHours = date.getHours() + remainingHours;
+	let endOfDayMinutes = date.getMinutes() + remainingMinutes;
+	
+	while (endOfDayMinutes >= 60){
+		endOfDayMinutes -= 60;
+		endOfDayHours += 1;
+	}
+	
+	return `Estimated end of day is ${prettyTime(endOfDayHours, endOfDayMinutes)}.`;
 }
 
 // Update displayed message
 // Need to add:
 //   How long you've worked (clocked in) (done)
-//   How long you've been on break (lunch break)
-//   How long you worked (post clock out)
+//   How long you've been on break (lunch break) (done)
+//   How long you worked (post clock out) (done)
 //   When you need to take next meal break (clocked in)
+//   How long you were on lunch break (after lunch break) (done)
+//   How long until 2nd lunch break
 function updateMessage() {
 	let timesheetLog = localStorage.getObj(storageKey);
 	let date = new Date();
 	if (timesheetLog.length === 0) {
 		let greeting;
-		if (date.getHours < 12) {
+		if (date.getHours() < 12) {
 			greeting = "Good morning!";
-		} else if (date.getHours < 17) {
+		} else if (date.getHours() < 17) {
 			greeting = "Good afternoon!";
 		} else {
 			greeting = "Good evening!";
@@ -115,10 +161,39 @@ function updateMessage() {
 		$('#currentMessage').html(`${greeting} You have not clocked in today.`);
 	} else {
 		let currentStatus = getLastItem(localStorage.getObj(storageKey))[0];
+		let [hours, minutes] = convertTimeToPrettyTime(calculateTimeWorked(timesheetLog, date));
+		let message;
 		if (currentStatus === dayIn) {
-			let [hours, minutes] = convertTimeToPrettyTime(calculateTimeWorked(timesheetLog, date));
-			$('#currentMessage').html(`You've worked for ${hours} hours and ${minutes} minutes today.`);
+			message = `You've worked for ${hours} hours and ${minutes} minutes today.`;
+			if (hours < 8) {
+				message += ` ${endOfDayMessage(hours, minutes, date, timesheetLog)}`;
+			}
+		} else if (currentStatus === mealOut) {
+			let [mealHours, mealMinutes] = convertTimeToPrettyTime(convertDateToMinutes(date)
+																	- parseStoredTime(getLastItem(timesheetLog)[1]));
+			if (mealHours > 0) {
+				message = `You've been on break for ${mealHours} hours and ${mealMinutes} minutes.`;
+			} else {
+				message = `You've been on break for ${mealMinutes} minutes.`;
+			}
+		} else if (currentStatus === mealIn) {
+			message = `You've worked for ${hours} hours and ${minutes} minutes today.`
+			let [mealHours, mealMinutes] = convertTimeToPrettyTime(parseStoredTime(getLastItem(timesheetLog)[1])
+																	- parseStoredTime(timesheetLog[timesheetLog.length - 2][1]));
+			if (mealHours > 0) {
+				message += ` Your last break was ${mealHours} hours and ${mealMinutes} minutes long.`;
+			} else {
+				message += ` Your last break was ${mealMinutes} minutes long.`;
+			}
+			
+			if (hours < 8) {
+				message += ` ${endOfDayMessage(hours, minutes, date, timesheetLog)}`;
+			}
+		} else if (currentStatus === dayOut) {
+			message = `You worked ${hours} hours and ${minutes} minutes during your last shift. Thanks for your hard work!`;
 		}
+		
+		$('#currentMessage').html(message);
 	}
 	
 }
